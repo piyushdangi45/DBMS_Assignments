bdbd472089801edafc7842ab9e8e78a7fbad5ff6
piyushdangi45 <piyushkumar58620@gmail.com>
2026-02-17 00:16:18 +0530

    Update Assignment_01_Solution.sql

--- Assignment_01/Assignment_01_Solution.sql
+++ Assignment_01/Assignment_01_Solution.sql
@@ -21,11 +21,11 @@ ALTER TABLE Student RENAME COLUMN Phone TO MobileNo;
 DROP TABLE Course;
 
 -- Inserting data into Table
-INSERT INTO Student VALUES (221, 'Sanskar', 'CSE', 21, '7698521455', 'Muzaffarpur', 5);
-INSERT INTO Student VALUES (598, 'Amarjeet', 'ME', 20, '9112233445', 'Calcutta', 5);
-INSERT INTO Student VALUES (564, 'Himanshu', 'EEE', 20, '6112365478', 'BiharSharif', 6);
-INSERT INTO Student VALUES (258, 'Ashu', 'ME', 22, '8001452369', 'Sitamarhi', 8);
-INSERT INTO Student VALUES (991, 'Anand', 'CSE', 18, '7225236545', 'Vaishali', 5);
+INSERT INTO Student VALUES (221, 'Prem', 'CSE', 21, '8512475845', 'Gaya', 6);
+INSERT INTO Student VALUES (598, 'Rahul', 'AI', 21, '9542845584', 'Patna', 4);
+INSERT INTO Student VALUES (564, 'Abhishek', 'EEE', 20, '6112365478', 'Madhepura', 6);
+INSERT INTO Student VALUES (258, 'Piyush', 'ME', 22, '858741258', 'Gaya', 7);
+INSERT INTO Student VALUES (991, 'Gajadhar', 'CSE', 18, '784125879', 'Patna', 5);
 
 -- Displaying Table details according to Question
 SELECT * FROM Student;
@@ -35,7 +35,7 @@ SELECT * FROM Student WHERE Age > 20 ;
 
 -- Modifying Table Entries 
 UPDATE Student SET Dept = 'ECE' WHERE RollNo = 564;
-UPDATE student SET city = 'Patna' WHERE name = 'Ashu';
+UPDATE student SET city = 'Patna' WHERE name = 'Rahul';
 UPDATE Student SET Age=Age+1;
 DELETE FROM Student WHERE RollNo = 991;
 
